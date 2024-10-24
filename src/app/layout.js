26e3140a6f66a6dd3a4128dd83021e09873bf364
Raraph84 <contact@raraph.fr>
2024-10-24 15:16:11 +0200

    Fix compiling

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Header } from "./layout/layout";
 
 import "./layout/globals.scss";
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
     title: {
         template: "%s | Money Manager",
